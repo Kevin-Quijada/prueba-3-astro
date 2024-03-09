448ad1230e4a19a43ddeb28056ce4e41c34d2464
kevin Quijada <kevinquijad@gmail.com>
2024-03-09 12:13:47 -0400

    cambios a los links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Kevin-Quijada.github.io',/* aqui colocarias el nombre de tu cuenta*/
+    base: '/prueba-3-astro',/* aqui colocarias el nombre de tu repositorio*/
+    build: {
+    assets: 'astro' /// muy importante
+    }
+    });
